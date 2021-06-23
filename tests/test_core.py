ca50af6e1cc7c1eeabfa06e7a07ccf5318c7510e
Keiichi <morytus@gmail.com>
2021-06-23 21:45:54 +0900

    Fix test for llt/core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,4 +3,4 @@ import unittest
 class LltTest(unittest.TestCase):
     def test_llt(self):
         from llt.core import llt
-        self.assertIsNone(llt())
+        self.assertIsNone(llt('cat', 'pro', 'tas', 'lab1,lab2'))
