9cda5b2894b4a955cf2e9ec7aea4a83f9430b09d
Keiichi <morytus@gmail.com>
2021-06-24 23:07:39 +0900

    Change target method

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 
 class LltTest(unittest.TestCase):
-    def test_llt(self):
+    def test_cmd(self):
         from llt.core import llt
-        self.assertIsNone(llt('cat', 'pro', 'tas', 'lab1,lab2'))
+        self.assertIsNone(llt('ok'))
+
+#    def test_start(self):
+#        from llt.core import start
+#        self.assertEqual(start(), 'start!')
