29e1b0db0804f735e01d78596fb3d654338fdc3e
Keiichi <morytus@gmail.com>
2021-06-25 00:40:01 +0900

    Use class

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from llt import core
+from llt.core import Core
+
+def main():
+    core = Core()
+    core.last_task()
+    core.cmd()
 
 if __name__ == '__main__':
-    core.cmd()
+    main()
